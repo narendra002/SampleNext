040f8eb30dc530bf8744755f32a1075067985080
Narendra Choudhary <narendrac024@gmail.com>
2023-11-12 10:39:51 +0000

    MakIng Respnsive Card

--- teaching_bee/src/components/Testimonials.jsx
+++ teaching_bee/src/components/Testimonials.jsx
@@ -13,45 +13,58 @@ const Testimonials = () => {
     autoplay: true,
     autoplaySpeed: 500, // Set the duration for each slide
   };
-  
+
   const responsiveSettings = [
     {
       breakpoint: 576, // Small devices (phones)
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 0.82,
       },
     },
     {
       breakpoint: 768, // Medium devices (tablets)
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 2,
       },
     },
     {
       breakpoint: 992, // Large devices (desktops)
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 2.5,
       },
     },
     {
       breakpoint: 1200, // Extra-large devices
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 3.3,
       },
     },
   ];
-  
+
+  // Dynamically calculate slidesToShow based on screen width
+  const getSlidesToShow = () => {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth < 576) {
+      return 0.7;
+    } else if (screenWidth < 768) {
+      return 2;
+    } else if (screenWidth < 992) {
+      return 2.5;
+    } else {
+      return 3.3;
+    }
+  };
+
   const settings = {
     ...commonSettings,
-    slidesToShow: 2.8,
+    slidesToShow: getSlidesToShow(),
     responsive: responsiveSettings,
   };
-  
 
   return (
     <div className='bg-white h-auto p-8 text-black'>
       <div className='max-w-5/6 mx-auto'>
-        {/* You can add additional styling or classes to this div if needed */}
         <Slider {...settings} className='shadow-sm'>
           {[1, 2, 3, 4].map((index) => (
             <div key={index}>
