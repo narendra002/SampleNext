a9454278d17be7197bd6b9ebfef9f0147574c142
Narendra Choudhary <narendrac024@gmail.com>
2023-11-14 13:42:32 +0000

    Change Color

--- teaching_bee/src/components/Blog.jsx
+++ teaching_bee/src/components/Blog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Blog() {
   return (
-    <div className="w-full md:w-2/6 lg:max-w-lg xl:max-w-xl mx-2 my-8 rounded-lg overflow-hidden shadow-2xl bg-gray-100 p-6">
+    <div className="w-full md:w-2/6 lg:max-w-lg xl:max-w-xl mx-4 my-8 rounded-lg overflow-hidden shadow-1xl border border-gray-200 bg-white p-6">
       <div className="flex items-center mb-4">
         <img className="w-12 h-12 rounded-full" src="https://img.icons8.com/clouds/100/user.png" alt="user" />
         <div className="ml-4 flex flex-col">
@@ -20,7 +20,7 @@ function Blog() {
         <img
           src='https://img.freepik.com/free-vector/flat-creativity-concept-illustration_52683-64279.jpg?w=740&t=st=1699875358~exp=1699875958~hmac=54889e085f1f57cc5cc85d2ef92efa8ea8fea03c339059c0ccabd1ac5a2ce1a6'
           alt="Blog Illustration"
-          className="w-full rounded-lg"
+          className="w-full rounded-lg mix-blend-multiply"
         />
       </div>
     </div>
